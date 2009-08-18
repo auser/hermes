dad9e726d1df6030dba0050cc7bbef3f33e81427
Ari Lerner <arilerner@mac.com>
2009-08-18 15:55:26 -0700

    Updated start script to work similarly to stoplights (thanks to jashmenn's suggestion)

--- start.sh
+++ start.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
+
+# Make the boot file, if necessary
 cd `dirname $0`
 if [[ ! -f ebin/hermes.boot ]]; then
 	make all_boot
 fi
-echo "erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name hermes -s reloader -boot hermes $*"
-erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -name hermes -s reloader -boot hermes $*
+
+# Find the next free erlang nodename using epmd  
+ 
+EXISTING_NAMES=`epmd -names`
+DEP_EBINS=`find deps -type d | grep -v \/test\/ | grep ebin | grep -v .svn | grep -v .git`
+ 
+CLIENT_NAME="hermes"
+ 
+exec erl \
+    -pa ebin \
+    -pa $DEP_EBINS \
+    -name $CLIENT_NAME \
+    -s reloader \
+    -boot hermes \
+    $*
