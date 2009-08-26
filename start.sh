3057372991d2df835c6a24104dad8c6614412716
Nate Murray <nate@natemurray.com>
2009-08-26 09:47:52 -0700

    updated loadpaths for stoplight

--- start.sh
+++ start.sh
@@ -10,6 +10,7 @@
 EXISTING_NAMES=`epmd -names`
 
 DEP_EBINS=`find deps -type d | grep -v \/test\/ | grep ebin | grep -v .svn | grep -v .git`
+INCLUDE_EBINS=`find include -type d | grep -v \/test\/ | grep ebin | grep -v .svn | grep -v .git`
 CLIENT_NAME="hermes"
 RELEASE=`ls -1 ebin/$CLIENT_NAME*.boot | sed 's/\(ebin\/hermes-\)\(.*\)\(\.boot\)/\2 \1\2\3/g' | awk -F '.' '{ ver=1000000*\$1 + 1000*\$2 + \$3; printf "%010d %s\n", ver, \$0}'| sort | awk '{print \$3}' | tail -n 1`
 APP_NAME=`basename $RELEASE .boot`
@@ -18,6 +19,7 @@ echo $APP_NAME
 exec erl \
     -pa ebin \
     -pa $DEP_EBINS \
+    -pa $INCLUDE_EBINS \
     -name $CLIENT_NAME \
     -s reloader \
     -boot $APP_NAME \
