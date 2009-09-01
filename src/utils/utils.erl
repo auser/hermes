64622d92bdee395f5f225634a865db21edfa81a5
Ari Lerner <arilerner@mac.com>
2009-09-01 15:26:03 -0700

    Fixed the nan use on turn_to_float

--- src/utils/utils.erl
+++ src/utils/utils.erl
@@ -43,14 +43,13 @@ turn_to_list(Arg) when is_list(Arg) -> [ turn_to_list(A) || A <- Arg ];
 turn_to_list(Bin) when is_binary(Bin) -> erlang:binary_to_list(Bin);
 turn_to_list(Arg) -> erlang:atom_to_list(Arg).
 
+turn_to_float("nan") -> 0.0;
 turn_to_float(Arg) when is_list(Arg) -> 
   case catch erlang:list_to_float(Arg) of
     {'EXIT',{badarg, Reason}} ->
-      io:format("Error: ~p (~p)~n", [Arg, regexp:match(Arg, " ")]),
       case regexp:match(Arg, " ") of
         {match, _, _} ->           
           {ok, Floats} = regexp:split(Arg, " "),
-          io:format("Floats: ~p~n", [Floats]),
           turn_to_float_from_list(Floats, []);
         _ -> ?TRACE("Uh oh. Error with float", [Arg, Reason])
       end;
