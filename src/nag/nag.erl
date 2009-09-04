22030cbc62e29a17bdcfb5ea14cfe2be88ddf46e
Ari Lerner <arilerner@mac.com>
2009-09-04 14:31:31 -0700

    Fixed logging erro

--- src/nag/nag.erl
+++ src/nag/nag.erl
@@ -92,10 +92,8 @@ handle_info({nag, Interval}, #state{sleep_delay = SleepDelay} = State) ->
     utils:turn_to_atom(LocalMon)
   end, utils:turn_to_list(MonReturn)),
   
-  ?INFO("Time to nag with monitors: ~p~n", [Monitors]),
-  ?ERROR("Monitors: ~p~n", [Monitors]),
-  
-  lists:map(fun(Mon) -> ?INFO("Monitor: ~p~n", [Mon]) end, [Monitors]),
+  ?INFO("Time to nag with monitors: ~p (~p)~n", [Monitors, length(Monitors)]),
+  lists:map(fun(Mon) -> ?INFO("Monitor: ~p~n", [Mon]) end, Monitors),
   
   
   lists:map(fun(Mon) ->
