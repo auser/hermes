7f5069abc7fafee7202d47caa2656960a685d9f2
Ari Lerner <arilerner@mac.com>
2009-09-03 18:19:55 -0700

    Removed output

--- src/ambassador/ambassadors/thrift_ambassador.erl
+++ src/ambassador/ambassadors/thrift_ambassador.erl
@@ -208,7 +208,7 @@ start_thrift_cloud_server(Args) ->
   end.  
   
 start_and_link_thrift_server(StartCmd) ->
-  ?INFO("Starting ~p: ~p => ~p~n", [?MODULE, StartCmd]),
+  ?INFO("Starting ~p: ~p~n", [?MODULE, StartCmd]),
   case spawn_link(fun() -> os:cmd(StartCmd) end) of
     {error, Reason} ->
       ?INFO("Assuming the thrift_client is already started error: ~p~n", [Reason]),
